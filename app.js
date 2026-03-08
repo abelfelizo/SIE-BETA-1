6bdbf16ad28b248336d5543ee805cb9ab2b869ab
abelfelizo <161610458+abelfelizo@users.noreply.github.com>
2026-03-07 20:11:52 -0400

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-// SIE 2028 v8.3 - app.js
-// Los datos están embebidos en index.html para GitHub Pages.
-// Este archivo está disponible para uso con servidor local o API futura.
-console.log('SIE 2028 v8.3 - Dataset 2024 ACTIVO');
